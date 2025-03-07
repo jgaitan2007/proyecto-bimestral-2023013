fed244d1e96c11fc4ab20b2abe700411e974e368
Informatica <informatica@alumno.local>
2025-03-07 10:48:50 -0600

    productos

--- src/gestionProductos/productos.controller.js
+++ src/gestionProductos/productos.controller.js
@@ -1,5 +1,6 @@
 import { hash } from "argon2";
 import productosModel from "./productos.model.js";
+import Categorias from "../gesitoncategorias/categorias.model.js";
 import fs from "fs/promises";
 import { fileURLToPath } from "url";
 
@@ -16,6 +17,110 @@ export const addProduct = async (req, res) => {
     } catch (err) {
         return res.status(500).json({
             message: "Product creation failed",
+            error: err.message
+        });
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    try {
+        const productData = req.body;
+        const { id } = req.params;
+
+        const updatedProduct = await productosModel.findByIdAndUpdate(id, productData, {
+            new: true
+        });
+
+        return res.status(200).json({
+            message: "Product has been updated",
+            product: updatedProduct
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Product update failed",
+            error: err.message
+        });
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const deletedProduct = await productosModel.findByIdAndDelete(id);
+
+        return res.status(200).json({
+            message: "Product has been deleted",
+            product: deletedProduct
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Product deletion failed",
+            error: err.message
+        });
+    }
+}
+
+export const listarProductos = async (req, res) => {
+    try {
+        const products = await productosModel.find();
+
+        return res.status(200).json({
+            message: "Product list fetched successfully",
+            products
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Product list fetch failed",
+            error: err.message
+        });
+    }
+}
+
+export const getProductById = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await productosModel.findById(id);
+
+        return res.status(200).json({
+            message: "Product fetched successfully",
+            product
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Product fetch failed",
+            error: err.message
+        });
+    }
+}
+
+export const listarFueraDeStock = async (req, res) => {
+    try {
+        const products = await productosModel.find({ stock: { $lt: 0 } });
+
+        return res.status(200).json({
+            message: "Product list fetched successfully",
+            products
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Product list fetch failed",
+            error: err.message
+        });
+    }
+}
+
+export const listarTopPoductos = async (req, res) => {
+    try {
+        const products = await productosModel.find().sort({ stock: -1 }).limit(5);
+
+        return res.status(200).json({
+            message: "Product list fetched successfully",
+            products
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: "Product list fetch failed",
             error: err.message
         });
     }
--- src/gestionProductos/productos.routes.js
+++ src/gestionProductos/productos.routes.js
@@ -1,15 +1,33 @@
 import { Router } from "express";
 import {
-     addProduct
+    addProduct,
+    listarProductos,
+    updateProduct,
+    deleteProduct,
+    getProductById,
+    listarFueraDeStock,
+    listarTopPoductos
+} from "./productos.controller.js";
+import {
+    addProductValidator,
+    updateProductValidator,
+    deleteProductValidator
+} from "../middlewares/productos-validator.js";
 
- } from "./productos.controller.js";
- import {
-    addProductValidator
- } from "../middlewares/productos-validator.js";
+const router = Router();
 
- const router = Router();
+router.post("/addProduct", addProductValidator, addProduct);
 
- router.post("/addProduct", addProductValidator, addProduct);
+router.get("/listarProductos", listarProductos);
 
- 
- export default router;
+router.get("/getProductById/:id", getProductById);
+
+router.put("/updateProduct/:id", updateProductValidator, updateProduct);
+
+router.delete("/deleteProduct/:id", deleteProductValidator, deleteProduct);
+
+router.get("/listarFueraDeStock", listarFueraDeStock);
+
+router.get("/listarTopPoductos", listarTopPoductos);
+
+export default router;
